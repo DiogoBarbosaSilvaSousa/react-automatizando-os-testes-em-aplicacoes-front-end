fa34ec8ff30269c082b7f17870746c4a43662981
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-06-18 00:50:21 -0300

    Upgrade code - Aula 2

--- projeto-inicial/src/App.test.js
+++ projeto-inicial/src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./app";
+import App, {calcularNovoSaldo} from "./app";
 
 describe("Componente principal", () => {
   describe("Quando eu abro o app do banco", () => {
@@ -19,4 +19,15 @@ describe("Componente principal", () => {
       expect(screen.getByText("Realizar operação")).toBeInTheDocument();
     });
   });
+});
+
+describe("Quando eu realizo uma transação", () => {
+    it("que é um saque, o valor vai diminuir", () => {
+        const valores = {
+            transacao: 'saque',
+            valor: 50
+        }
+        const novoSaldo = calcularNovoSaldo(valores, 150);
+        expect(novoSaldo).toBe(100);
+    });
 });
